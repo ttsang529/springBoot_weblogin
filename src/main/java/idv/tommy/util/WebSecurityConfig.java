5c4a2bd1443a4a8a59b340c5357f9fc999333d8d
Tommy <tommy_tseng@ubiqconn.com>
2020-01-22 11:48:50 +0800

    image mysql db control login account

--- src/main/java/idv/tommy/util/WebSecurityConfig.java
+++ src/main/java/idv/tommy/util/WebSecurityConfig.java
@@ -1,28 +1,27 @@
 package idv.tommy.util;
 
-import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 
 
+//		http
+//		.csrf().ignoringAntMatchers("/admin/****","/user/**","/app/**");
 		http
 		.csrf().disable()
 		.authorizeRequests()
@@ -46,14 +45,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	    web.ignoring().antMatchers("/webjars/**/*", "/**/*.css", "/**/*.js");
 	}
 	
-	//password
-	private static final String USER_ENCODED_PASSWORD  = "$2a$10$tjN.z9gCogdj80v3sHMWfezQ55nlrWNUSuqbI3Hv4dH0IVDRIW1rK";
-////For Single user password
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
 
+////For Single user password
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//	    return new BCryptPasswordEncoder();
+//	}
+	 
+	@Autowired 
+    @Qualifier("dataSource")
+    private DataSource dataSource;
+	
+	@Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+            .usersByUsernameQuery("select username, password, enabled"
+                + " from users where username=?")
+            .authoritiesByUsernameQuery("select username, authority "
+                + "from authorities where username=?")
+            .passwordEncoder(new BCryptPasswordEncoder());
+    }
+	
+	
+////password(First method implement)
+//	private static final String USER_ENCODED_PASSWORD  = "$2a$10$tjN.z9gCogdj80v3sHMWfezQ55nlrWNUSuqbI3Hv4dH0IVDRIW1rK";	
 //	@Override
 //	protected void configure(AuthenticationManagerBuilder auth) throws Exception {	
 //	    auth.inMemoryAuthentication()
@@ -63,16 +78,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //			.withUser("admin").password("USER_ENCODED_PASSWORD").roles("ADMIN","USER");;
 //	}
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		String password = passwordEncoder.encode("password");
-		InMemoryUserDetailsManager  manager = new InMemoryUserDetailsManager();
-		manager.createUser( User.withUsername("user").password(password).roles("USER").build());
-		manager.createUser( User.withUsername("admin").password(password).roles("ADMIN","USER").build());
-		
-		return manager;
-	}
+////Other methods to study implement insert default date
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//		String password = passwordEncoder.encode("password");
+//		InMemoryUserDetailsManager  manager = new InMemoryUserDetailsManager();
+//		manager.createUser( User.withUsername("user").password(password).roles("USER").build());
+//		manager.createUser( User.withUsername("admin").password(password).roles("ADMIN","USER").build());
+//		
+//		return manager;
+//	}
 	
 
 
